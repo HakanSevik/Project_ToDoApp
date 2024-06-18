dc84745ba8d76b81d8a35931205721c044d43ad7
DavidVukasovic <210117@studierende.htl-donaustadt.at>
2024-06-18 11:06:06 +0200

    Login update/ Screen fix

--- todoapp_2/todoapp/todoapp/lib/main.dart
+++ todoapp_2/todoapp/todoapp/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/widget_tree.dart';
+import 'firebase_options.dart'; 
+
 
 Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Diese Zeile gibt die Firebase-Optionen an
+  );
   runApp(const MyApp());
 }
 
--- todoapp_2/todoapp/todoapp/lib/pages/login_register_page.dart
+++ todoapp_2/todoapp/todoapp/lib/pages/login_register_page.dart
@@ -1,108 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../auth.dart';
+import '../screens/home.dart';
 
-class LoginPage extends StatefulWidget{
-  const LoginPage({Key? key}) : super(key:key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-  @override 
+  @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String? errorMessage ='';
-  bool isLogin=true;
+  String? errorMessage = '';
+  bool isLogin = true;
 
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
 
-Future <void> signInWithEmailAndPassword() async{
-  try {
-    await Auth().signInWithEmailAndPassword(
-      email: _controllerEmail.text,
-      password: _controllerPassword.text,
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
       );
-  } on FirebaseAuthException catch(e){
-    setState(() {
-      errorMessage = e.message;
-    });
-  }
-}
 
-Future <void> createUserWithEmailAndPassword() async{
-  try{
-    await Auth().createUserWithEmailAndPassword(
-      email: _controllerEmail.text, 
-      password: _controllerPassword.text,
-      );
-  } on FirebaseAuthException catch(e){
-    setState(() {
-      errorMessage = e.message;
-    });
-  }
-}
-
-Widget _title(){
-  return const Text('Firebase Auth');
-}
-
-Widget _entryField( 
-  String title,
-  TextEditingController controller,
-  ){
-  return TextField(
-    controller: controller,
-    decoration: InputDecoration(
-      labelText: title,
-    ),
-  );
-}
-
-Widget _errorMessage(){
-  return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage');
-}
-
-Widget _submitButton(){
-  return ElevatedButton(
-    onPressed:
-        isLogin ? signInWithEmailAndPassword: createUserWithEmailAndPassword,
-    child: Text(isLogin ? 'Login' : 'Register'),
-    );
-}
-
-Widget _loginOrRegisterButton(){
-  return TextButton(
-    onPressed: (){
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    } on FirebaseAuthException catch (e) {
       setState(() {
-        isLogin = !isLogin;
+        errorMessage = e.message;
       });
-    },
-    child: Text(isLogin ? 'Register instead' : 'Login instead'),
-    );
-}
+    }
+  }
 
-  @override 
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Widget _title() {
+    return const Text('Firebase Auth');
+  }
+
+  Widget _entryField(
+    String title,
+    TextEditingController controller,
+  ) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: title,
+      ),
+    );
+  }
+
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : 'Humm ? $errorMessage');
+  }
+
+  Widget _submitButton() {
+    return ElevatedButton(
+      onPressed:
+          isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+      child: Text(isLogin ? 'Login' : 'Register'),
+    );
+  }
+
+  Widget _loginOrRegisterButton() {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          isLogin = !isLogin;
+        });
+      },
+      child: Text(isLogin ? 'Register instead' : 'Login instead'),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: _title(),
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _entryField('email', _controllerEmail),
-            _entryField('password', _controllerPassword),
-            _errorMessage(),
-            _submitButton(),
-            _loginOrRegisterButton()
-          ],
-        )
-      )
-    );
+        appBar: AppBar(
+          title: _title(),
+        ),
+        body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _entryField('email', _controllerEmail),
+                _entryField('password', _controllerPassword),
+                _errorMessage(),
+                _submitButton(),
+                _loginOrRegisterButton()
+              ],
+            )));
   }
 }
